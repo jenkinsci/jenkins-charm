733c0dad4ff4d74f3a379e992bf040d3fd022eb9
alejdg <alexandre.gomes@canonical.com>
2021-03-03 09:36:55 -0800

    Remove duplicated test case

--- unit_tests/test_api.py
+++ unit_tests/test_api.py
@@ -310,17 +310,6 @@ class ApiTest(JenkinsTest):
         The set_update_center() method runs a groovy script to modify the
         update center url to default when no url value is given.
         """
-        url = "https://updates.jenkins.io/stable/update-center.json"
-        script = SET_UPDATE_CENTER_SCRIPT.format(
-            url=url)
-        self.fakes.jenkins.scripts[script] = ""
-        self.assertIsNone(self.api.set_update_center())
-
-    def test_check_update_center(self):
-        """
-        The set_update_center() method runs a groovy script to modify the
-        update center url to default when no url value is given.
-        """
         script = (
             "Jenkins.instance.getUpdateCenter().getSites().each { site ->"
             "  site.updateDirectlyNow(hudson.model.DownloadService.signatureCheck)"
