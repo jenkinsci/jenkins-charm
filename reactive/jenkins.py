9b9dd7972cb59699e534a0ef31bd5705242d8734
alejdg <alexandre.gomes@canonical.com>
2021-03-09 16:02:42 -0800

    Remove old plugins cleanup

--- reactive/jenkins.py
+++ reactive/jenkins.py
@@ -102,7 +102,6 @@ def upgrade_jenkins():
             packages.install_jenkins()
             api = Api()
             api.wait()  # Wait for the upgrade to finish
-            packages.clean_old_plugins()
             unitdata.kv().set("jenkins.plugins.last_update", 0)
             update_plugins()
         else:
