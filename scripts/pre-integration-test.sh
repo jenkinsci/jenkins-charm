913362f8c104a7ef76c82e16e5a2b6c01090346c
Andrew Liaw <yhaliaw@example.com>
2022-11-24 15:28:50 +0800

    Remove update-alterinatives.

--- scripts/pre-integration-test.sh
+++ scripts/pre-integration-test.sh
@@ -5,7 +5,3 @@
 
 # [2022-09-29] Need to install Python 3.8 because that is what the tests run on and 18.04
 sudo apt install python3.8
-sudo update-alternatives --install /usr/local/bin/python python3 /usr/bin/python3.8 100
-sudo update-alternatives --set python3 /usr/bin/python3.8
-sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3.8 100
-sudo update-alternatives --set python /usr/bin/python3.8
