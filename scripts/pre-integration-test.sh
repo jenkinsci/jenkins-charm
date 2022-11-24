b76e70ce3f0123fb43acc36d449c143b26d14922
Andrew Liaw <yhaliaw@example.com>
2022-11-24 15:07:16 +0800

    Add script to install python 3.8 for integration test.

---
+++ scripts/pre-integration-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Pre-run script for integration test operator-workflows action.
+# https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
+
+sudo apt install python3.8
