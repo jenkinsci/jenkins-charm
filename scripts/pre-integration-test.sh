407c1a8fc271bd49c9c8fa1e914bc1e288542ee7
Andrew Liaw <yhaliaw@example.com>
2022-11-25 08:32:38 +0800

    Revert back to before using operator-workflows.

--- scripts/pre-integration-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Pre-run script for integration test operator-workflows action.
-# https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
-
-# [2022-09-29] Need to install Python 3.8 because that is what the tests run on and 18.04
-sudo apt install python3.8
